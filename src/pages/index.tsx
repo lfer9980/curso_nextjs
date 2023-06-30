718076282988b9f27ccc4c0acca858379ceb7a02
lfer9980 <luis_fernandez99@hotmail.com>
2023-06-29 22:37:01 -0600

    :art: Creando y consumiendo api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,25 @@ import React from 'react'
 import Navbar from '@/components/Navbar/Navbar'
 
 const HomePage = () => {
+
+  const [productList, setProductList] = React.useState<TProduct[]>([])
+
+  React.useEffect(() => {
+    window.fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data, length }) => {
+        setProductList(data);
+      })
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <div>Platzi and Next.js!</div>
+      {productList.map((product, i) => (
+        <div key={i}>
+          {product.name}
+        </div>
+      ))}
     </div>
   )
 }
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,11 +2,21 @@ import React from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
-  const { query } = useRouter()
+  const { query } = useRouter();
+  const [product, setProduct] = React.useState<TProduct>()
+
+  React.useEffect(() => {
+    window.fetch(`/api/avo/${query.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setProduct(data)
+      })
+  }, []);
 
   return (
     <section>
       <h1>Página producto: {query.id}</h1>
+      {product?.name}
     </section>
   )
 }
