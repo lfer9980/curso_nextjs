7cfd3b0f15259ed6579768d417c60a0d5843aa80
lfer9980 <luis_fernandez99@hotmail.com>
2023-06-29 22:52:38 -0600

    :art: Extendiendo el Document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+	return (
+	  <Html lang="es">
+		<Head>
+			{/* favicon */}
+			{/* webfont */}
+			{/* stylesheets from internet */}
+			{/* otros scripts de js de internet */}
+		</Head>
+		<body className="my-body-class">
+		  <Main />
+		  <NextScript />
+		</body>
+	  </Html>
+	)
+  }
