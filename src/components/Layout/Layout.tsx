67b7c578101396ee3d9ff42b1cdeff07d2c90a03
lfer9980 <luis_fernandez99@hotmail.com>
2023-07-03 23:06:16 -0600

    :art: Clase 15: css con nextjs

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react';
-import NavBar from '../Navbar/Navbar';
+import NavBar from '@components/Navbar/Navbar';
+
+/* import styles from "@/styles/style.module.css"; */
 
 function Layout({ children }: Props) {
 	return (
-		<main>
+		<main className='container'>
 			<NavBar />
 			{children}
 			<footer>
 				this is the footer
 			</footer>
+			<style jsx>
+				{
+					/* template literals CSS in JS */
+					`
+					.container {
+						background: salmon;
+					}
+					`
+				}
+
+			</style>
 		</main>
 	);
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { AppProps } from "next/app";
 import { Layout } from "@/components/Layout/Layout";
 
+/* importar CSS de forma global */
+/* import "@/styles/style.css"; */
+
 export default function MyApp({ Component, pageProps }: AppProps) {
 	/* componente perfecto para utilizar providers / contexts */
 	/* agregar layouts */
